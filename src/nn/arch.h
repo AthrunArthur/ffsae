c8f1eb79c6cb51d2780e467c6617431039f5d3d0
Xuepeng Fan <athrunarthur@gmail.com>
2014-05-06 04:38:07 +0800

    arch

---
+++ src/nn/arch.h
@@ -0,0 +1,11 @@
+#ifndef FFSAE_NN_NN_ARCH_H_
+#define FFSAE_NN_NN_ARCH_H_
+
+#include <blaze/Math.h>
+
+namespace ff{
+  typedef blaze::StaticVector<int, 3UL, blaze::columnVector> Arch_t;
+
+}
+;//end namespace ff
+#endif
