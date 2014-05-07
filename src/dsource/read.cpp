e0c93879dfcad2b0f6cad24c54a157fc5b88da21
Xuepeng Fan <athrunarthur@gmail.com>
2014-05-07 15:26:55 +0800

    chiuchiu

--- src/dsource/read.cpp
+++ src/dsource/read.cpp
@@ -13,13 +13,22 @@ namespace ff
   static FMatrix_ptr read_matrix_in_mat(mat_t * mat, const char *varname)
   {
     matvar_t  * tvar = Mat_VarRead(mat, varname);
-    FMatrix_ptr res;
-
+    int rows = tvar->dims[0];
+    int columns = tvar->dims[1];
+    uint8_t * pData = static_cast<uint8_t *>(tvar->data);
+    FMatrix_ptr res = std::make_shared<FMatrix>(rows, columns);
+    for(int k = 0; k < rows; ++k)
+    {
+      for(int i = 0; i < columns; ++i)
+      {
+        res->operator()(k, i) = static_cast<double>(pData[rows * i + k]);
+      }
+    }
     return res;
   }
   TData         read_data()
   {
-    const char * fileName = "../data/mnist_unit8.mat";
+    const char * fileName = "../data/mnist_uint8.mat";
     TData res;
     mat_t *mat = nullptr;
     scope_guard _l([&mat, fileName](){
@@ -27,20 +36,39 @@ namespace ff
       },
     [&mat](){Mat_Close(mat); });
     if(!mat){
+      std::cout <<"cannot open "<<fileName<<std::endl;
       return res;
     }  
     matvar_t *var = 0;
-    matvar_t *tvar = 0;
+    int read_items = 0;
     while((var = Mat_VarReadNextInfo(mat)) != nullptr){
       if(strcmp(var->name, "train_x") == 0)
       {
         res.train_x = read_matrix_in_mat(mat, var->name);
+        read_items ++ ;
       }
       else if(strcmp(var->name, "train_y") == 0)
       {
-        
+        res.train_y = read_matrix_in_mat(mat, var->name);
+        read_items ++;
       }
-
+      else if(strcmp(var->name, "test_x") == 0)
+      {
+        res.test_x = read_matrix_in_mat(mat, var->name);
+        read_items ++;
+      }
+      else if(strcmp(var->name, "test_y" ) == 0)
+      {
+        res.test_y = read_matrix_in_mat(mat, var->name);
+        read_items ++;
+      }
+      else{
+        //nothing here, just for fun...
+      }
+    }
+    if(read_items != 4)
+    {
+      std::cout<<"ooooooops, cannot read "<< fileName<<std::endl;
     }
     return res;
   }
---
+++ tests/test_dsource.cpp
@@ -0,0 +1,14 @@
+#include "dsource/read.h"
+
+using namespace ff;
+int main(int argc, char *argv[])
+{
+  TData d = read_data();
+  std::cout<<"train_x : "<<std::endl;
+  std::cout<<"\t rows: "<<d.train_x->rows()<<std::endl;
+  std::cout<<"\t cols: "<<d.train_x->columns()<<std::endl;
+  std::cout<<"train_y : "<<std::endl;
+  std::cout<<"\t rows: " <<d.train_y->rows()<<std::endl;
+  std::cout<<"\t cols: "<<d.train_y->rows() <<std::endl;
+  return 0;
+}
